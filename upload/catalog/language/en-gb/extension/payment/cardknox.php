e2465362be2fc627a7d393ace2b3cf9bbf64c8cc
Joe Rothrock <rothrockindustries@gmail.com>
2021-06-01 18:13:32 -0400

    Updated to have address on card input page; Moved javascript to main checkout page.

--- upload/catalog/language/en-gb/extension/payment/cardknox.php
+++ upload/catalog/language/en-gb/extension/payment/cardknox.php
@@ -15,6 +15,8 @@ $_['entry_cc_cvv2']        = 'Card Security Code (CVV2)';
 $_['entry_cc_save']        = 'Save card for future orders';
 $_['entry_cc_saved']       = 'Use Saved card';
 $_['entry_cc_new']         = 'New card';
+$_['entry_cc_address']     = 'Card Billing Street Address';
+$_['entry_cc_zip']         = 'Card Billing Zip';
 
 // Errors
 $_['error_save']		   = 'Card Save Error!';
