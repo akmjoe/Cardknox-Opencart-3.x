5901cb7b5560020431f97adfb4b468fbc1e203f6
Joe Rothrock <rothrockindustries@gmail.com>
2021-06-28 11:01:29 -0400

    Added captcha to card entry page

--- upload/catalog/controller/extension/payment/cardknox.php
+++ upload/catalog/controller/extension/payment/cardknox.php
@@ -52,96 +52,115 @@ class ControllerExtensionPaymentCardknox extends Controller {
 		$data['cc_address'] = $order_info['payment_address_1'];
 		$data['cc_zip'] = $order_info['payment_postcode'];
 		// $this->document->addScript('https://cdn.cardknox.com/ifields/ifields.min.js');
+		/***************** Add Captcha *****************************/
+		if ($this->config->get('captcha_' . $this->config->get('config_captcha') . '_status') && in_array('card', (array)$this->config->get('config_captcha_page'))) {
+			unset($this->session->data['gcapcha']);
+			$data['captcha'] = $this->load->controller('extension/captcha/' . $this->config->get('config_captcha'));
+		} else {
+			$data['captcha'] = '';
+		}
+		
 		return $this->load->view('extension/payment/cardknox', $data);
 	}
 
 	public function send() {
 		$url = 'https://x1.cardknox.com/gateway';
-		/*********************** First save address ****************************/
-		
+		/*********************** Check Captcha ****************************/
+		// Captcha
+		if ($this->config->get('captcha_' . $this->config->get('config_captcha') . '_status') && in_array('card', (array)$this->config->get('config_captcha_page'))) {
+			$captcha = $this->load->controller('extension/captcha/' . $this->config->get('config_captcha') . '/validate');
 
-		$this->load->model('checkout/order');
-
-		$order_info = $this->model_checkout_order->getOrder($this->session->data['order_id']);
-
-		$data = array();
-		$json = array();
-
-		$data['xKey'] = $this->config->get('payment_cardknox_transaction_key');
-		$data['xVersion'] = '4.5.8';
-		$data['xSoftwareName'] = 'OpenCart';
-		$data['xSoftwareVersion'] = '1.0.2';
-
-		$data['xBillFirstname'] = html_entity_decode($order_info['payment_firstname'], ENT_QUOTES, 'UTF-8');
-		$data['xBillLastname'] = html_entity_decode($order_info['payment_lastname'], ENT_QUOTES, 'UTF-8');
-		$data['xBillCompany'] = html_entity_decode($order_info['payment_company'], ENT_QUOTES, 'UTF-8');
-		$data['xBillCountry'] = html_entity_decode($order_info['payment_country'], ENT_QUOTES, 'UTF-8');
-		$data['xBillPhone'] = $order_info['telephone'];
-		$data['xIP'] = $this->request->server['REMOTE_ADDR'];
-		$data['xEmail'] = $order_info['email'];
-		$data['xDescription'] = html_entity_decode($this->config->get('config_name'), ENT_QUOTES, 'UTF-8');
-		$data['xAmount'] = $this->currency->format($order_info['total'], $order_info['currency_code'], 1.00000, false);
-		$data['xCurrency'] = $this->session->data['currency'];
-		$data['xCommand'] = ($this->config->get('payment_cardknox_method') == 'capture') ? 'cc:sale' : 'cc:authonly';
-		if($this->request->post['cc_from'] == 'existing' && $this->request->post['cc_id']) {
-			$this->load->model('extension/credit_card/cardknox');
-			$id = (int)$this->request->post['cc_id'];
-			$data['xToken'] = $this->model_extension_credit_card_cardknox->getToken($this->customer->getID(), $id);
-		} else {// new card
-			$data['xBillStreet'] = html_entity_decode($order_info['payment_address_1'], ENT_QUOTES, 'UTF-8');
-			$data['xBillCity'] = html_entity_decode($order_info['payment_city'], ENT_QUOTES, 'UTF-8');
-			$data['xBillState'] = html_entity_decode($order_info['payment_zone'], ENT_QUOTES, 'UTF-8');
-			$data['xBillZip'] = html_entity_decode($order_info['payment_postcode'], ENT_QUOTES, 'UTF-8');
-			$id = $this->saveCard();
-			if(!$id) {
-				// card failed to save
-				$json['error'] = $this->language->get('error_card');
-			} else {
-				$result = $this->db->query('select * from '.DB_PREFIX.'cardknox_card where customer_card_id = '.(int)$id);
-				if($result->row['status'] == 1 || $result->row['status'] == 2) {
-					// valid card
-					$data['xToken'] = $result->row['token'];
-					// save ID for split checkout
-					$this->session->data['card_id'] = $id;
-				} else {
-					// failed AVS or card is disabled
-					$json['error'] = $this->language->get('error_address');
-				}
+			if ($captcha) {
+				$json['error']['captcha'] = $captcha;
 			}
-			//$data['xCardNum'] = $this->request->post['xCardNum'];
-			$data['xExp'] = $this->request->post['cc_expire_date_month'] . substr($this->request->post['cc_expire_date_year'],2,2);
-			$data['xCVV'] = $this->request->post['xCVV'];
 		}
-		$data['xInvoice'] = $this->session->data['order_id'];
+		/*********************** Now save address ****************************/
+		if(!isset($json['error'])) {
 
-		/* Customer Shipping Address Fields */
-		if ($order_info['shipping_method']) {
-			$data['xShipFirstname'] = html_entity_decode($order_info['shipping_firstname'], ENT_QUOTES, 'UTF-8');
-			$data['xShipLastname'] = html_entity_decode($order_info['shipping_lastname'], ENT_QUOTES, 'UTF-8');
-			$data['xShipCompany'] = html_entity_decode($order_info['shipping_company'], ENT_QUOTES, 'UTF-8');
-			$data['xShipStreet'] = html_entity_decode($order_info['shipping_address_1'], ENT_QUOTES, 'UTF-8') . ' ' . html_entity_decode($order_info['shipping_address_2'], ENT_QUOTES, 'UTF-8');
-			$data['xShipCity'] = html_entity_decode($order_info['shipping_city'], ENT_QUOTES, 'UTF-8');
-			$data['xShipState'] = html_entity_decode($order_info['shipping_zone'], ENT_QUOTES, 'UTF-8');
-			$data['xShipZip'] = html_entity_decode($order_info['shipping_postcode'], ENT_QUOTES, 'UTF-8');
-			$data['xShipCountry'] = html_entity_decode($order_info['shipping_country'], ENT_QUOTES, 'UTF-8');
-		} else {
-			$data['xShipFirstname'] = html_entity_decode($order_info['payment_firstname'], ENT_QUOTES, 'UTF-8');
-			$data['xShipLastname'] = html_entity_decode($order_info['payment_lastname'], ENT_QUOTES, 'UTF-8');
-			$data['xShipCompany'] = html_entity_decode($order_info['payment_company'], ENT_QUOTES, 'UTF-8');
-			$data['xShipStreet'] = html_entity_decode($order_info['payment_address_1'], ENT_QUOTES, 'UTF-8') . ' ' . html_entity_decode($order_info['payment_address_2'], ENT_QUOTES, 'UTF-8');
-			$data['xShipCity'] = html_entity_decode($order_info['payment_city'], ENT_QUOTES, 'UTF-8');
-			$data['xShipState'] = html_entity_decode($order_info['payment_zone'], ENT_QUOTES, 'UTF-8');
-			$data['xShipZip'] = html_entity_decode($order_info['payment_postcode'], ENT_QUOTES, 'UTF-8');
-			$data['xShipCountry'] = html_entity_decode($order_info['payment_country'], ENT_QUOTES, 'UTF-8');
-		}
-		
-		$replacements = array('xKey' => "******", 'xCardNum' => "******", 'xCVV' => "******");	
+			$this->load->model('checkout/order');
 
-		$this->logger(array_replace($data, $replacements));
-		
-		if($this->config->get('payment_cardknox_debug')) {
-			$this->log->write($this->request->post);
+			$order_info = $this->model_checkout_order->getOrder($this->session->data['order_id']);
+
+			$data = array();
+			$json = array();
+
+			$data['xKey'] = $this->config->get('payment_cardknox_transaction_key');
+			$data['xVersion'] = '4.5.8';
+			$data['xSoftwareName'] = 'OpenCart';
+			$data['xSoftwareVersion'] = '1.0.2';
+
+			$data['xBillFirstname'] = html_entity_decode($order_info['payment_firstname'], ENT_QUOTES, 'UTF-8');
+			$data['xBillLastname'] = html_entity_decode($order_info['payment_lastname'], ENT_QUOTES, 'UTF-8');
+			$data['xBillCompany'] = html_entity_decode($order_info['payment_company'], ENT_QUOTES, 'UTF-8');
+			$data['xBillCountry'] = html_entity_decode($order_info['payment_country'], ENT_QUOTES, 'UTF-8');
+			$data['xBillPhone'] = $order_info['telephone'];
+			$data['xIP'] = $this->request->server['REMOTE_ADDR'];
+			$data['xEmail'] = $order_info['email'];
+			$data['xDescription'] = html_entity_decode($this->config->get('config_name'), ENT_QUOTES, 'UTF-8');
+			$data['xAmount'] = $this->currency->format($order_info['total'], $order_info['currency_code'], 1.00000, false);
+			$data['xCurrency'] = $this->session->data['currency'];
+			$data['xCommand'] = ($this->config->get('payment_cardknox_method') == 'capture') ? 'cc:sale' : 'cc:authonly';
+			if($this->request->post['cc_from'] == 'existing' && $this->request->post['cc_id']) {
+				$this->load->model('extension/credit_card/cardknox');
+				$id = (int)$this->request->post['cc_id'];
+				$data['xToken'] = $this->model_extension_credit_card_cardknox->getToken($this->customer->getID(), $id);
+			} else {// new card
+				$data['xBillStreet'] = html_entity_decode($order_info['payment_address_1'], ENT_QUOTES, 'UTF-8');
+				$data['xBillCity'] = html_entity_decode($order_info['payment_city'], ENT_QUOTES, 'UTF-8');
+				$data['xBillState'] = html_entity_decode($order_info['payment_zone'], ENT_QUOTES, 'UTF-8');
+				$data['xBillZip'] = html_entity_decode($order_info['payment_postcode'], ENT_QUOTES, 'UTF-8');
+				$id = $this->saveCard();
+				if(!$id) {
+					// card failed to save
+					$json['error'] = $this->language->get('error_card');
+				} else {
+					$result = $this->db->query('select * from '.DB_PREFIX.'cardknox_card where customer_card_id = '.(int)$id);
+					if($result->row['status'] == 1 || $result->row['status'] == 2) {
+						// valid card
+						$data['xToken'] = $result->row['token'];
+						// save ID for split checkout
+						$this->session->data['card_id'] = $id;
+					} else {
+						// failed AVS or card is disabled
+						$json['error'] = $this->language->get('error_address');
+					}
+				}
+				//$data['xCardNum'] = $this->request->post['xCardNum'];
+				$data['xExp'] = $this->request->post['cc_expire_date_month'] . substr($this->request->post['cc_expire_date_year'],2,2);
+				$data['xCVV'] = $this->request->post['xCVV'];
+			}
+			$data['xInvoice'] = $this->session->data['order_id'];
+
+			/* Customer Shipping Address Fields */
+			if ($order_info['shipping_method']) {
+				$data['xShipFirstname'] = html_entity_decode($order_info['shipping_firstname'], ENT_QUOTES, 'UTF-8');
+				$data['xShipLastname'] = html_entity_decode($order_info['shipping_lastname'], ENT_QUOTES, 'UTF-8');
+				$data['xShipCompany'] = html_entity_decode($order_info['shipping_company'], ENT_QUOTES, 'UTF-8');
+				$data['xShipStreet'] = html_entity_decode($order_info['shipping_address_1'], ENT_QUOTES, 'UTF-8') . ' ' . html_entity_decode($order_info['shipping_address_2'], ENT_QUOTES, 'UTF-8');
+				$data['xShipCity'] = html_entity_decode($order_info['shipping_city'], ENT_QUOTES, 'UTF-8');
+				$data['xShipState'] = html_entity_decode($order_info['shipping_zone'], ENT_QUOTES, 'UTF-8');
+				$data['xShipZip'] = html_entity_decode($order_info['shipping_postcode'], ENT_QUOTES, 'UTF-8');
+				$data['xShipCountry'] = html_entity_decode($order_info['shipping_country'], ENT_QUOTES, 'UTF-8');
+			} else {
+				$data['xShipFirstname'] = html_entity_decode($order_info['payment_firstname'], ENT_QUOTES, 'UTF-8');
+				$data['xShipLastname'] = html_entity_decode($order_info['payment_lastname'], ENT_QUOTES, 'UTF-8');
+				$data['xShipCompany'] = html_entity_decode($order_info['payment_company'], ENT_QUOTES, 'UTF-8');
+				$data['xShipStreet'] = html_entity_decode($order_info['payment_address_1'], ENT_QUOTES, 'UTF-8') . ' ' . html_entity_decode($order_info['payment_address_2'], ENT_QUOTES, 'UTF-8');
+				$data['xShipCity'] = html_entity_decode($order_info['payment_city'], ENT_QUOTES, 'UTF-8');
+				$data['xShipState'] = html_entity_decode($order_info['payment_zone'], ENT_QUOTES, 'UTF-8');
+				$data['xShipZip'] = html_entity_decode($order_info['payment_postcode'], ENT_QUOTES, 'UTF-8');
+				$data['xShipCountry'] = html_entity_decode($order_info['payment_country'], ENT_QUOTES, 'UTF-8');
+			}
+
+			$replacements = array('xKey' => "******", 'xCardNum' => "******", 'xCVV' => "******");	
+
+			$this->logger(array_replace($data, $replacements));
+
+			if($this->config->get('payment_cardknox_debug')) {
+				$this->log->write($this->request->post);
+			}
 		}
+		/******************* Process card **********************/
 		if(!isset($json['error']) && isset($data['xToken']) && $data['xToken']) {
 			// process card
 			$curl = curl_init($url);
